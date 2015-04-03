c0f77902fdc2f6f78027dfe92f3ac5db16bf3418
Imanol Aranzadi <iiaranzadi@gmail.com>
2015-04-03 14:25:58 -0700

    changed items design

--- farmsList/farmsList/static/js/app.js
+++ farmsList/farmsList/static/js/app.js
@@ -7,24 +7,36 @@ angular.module('listApp', [])
   $scope.farms = [
     {
       "title": "Jordan Valley",
-      "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo, nostrum, quis. Eos labore dignissimos provident illum quisquam tempora quasi nemo magnam at beatae consequatur debitis eum modi molestiae, temporibus accusamus! ",
-      "contact": "Jordan Gregory",
+      "contact": "Diego Aranzadi",
       "phone": "530-555-1234",
       "address": "Km 41.2 West Sacramento, CA 95605",
+      "geolocation": ['longitude', 'latitutde'],
+      "size": 2.1,
+      "zone": "type of zone",
+      "water": "Yes - potable",
+      "developmentPlans": 2,
       "image": "http://www.moraitis.com.au/media/images/North-Queensland-Potato-Farm-e37f75d1-b49f-4a7a-954a-5daf8ac8fd56-0-2080x1544.JPG"
     },{
-      "title": "Jordan Valley",
-      "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo, nostrum, quis. Eos labore dignissimos provident illum quisquam tempora quasi nemo magnam at beatae consequatur debitis eum modi molestiae, temporibus accusamus! ",
-      "contact": "Jordan Gregory",
+      "title": "Yolo Valley",
+      "contact": "Ivan Matos",
       "phone": "530-555-1234",
       "address": "Km 41.2 West Sacramento, CA 95605",
+      "geolocation": ['longitude', 'latitutde'],
+      "size": 2.3,
+      "zone": "type of zone",
+      "water": "Yes - potable",
+      "developmentPlans": 2,
       "image": "http://www.farmsunday.org/resources/000/746/284/Rowley_Farm.JPG"
     },{
-      "title": "Jordan Valley",
-      "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo, nostrum, quis. Eos labore dignissimos provident illum quisquam tempora quasi nemo magnam at beatae consequatur debitis eum modi molestiae, temporibus accusamus! ",
-      "contact": "Jordan Gregory",
+      "title": "Old McDonald Farm",
+      "contact": "Felix Colón",
       "phone": "530-555-1234",
       "address": "Km 41.2 West Sacramento, CA 95605",
+      "geolocation": ['longitude', 'latitutde'],
+      "size": 1.8,
+      "zone": "type of zone",
+      "water": "Yes - potable",
+      "developmentPlans": 2,
       "image": "http://broadrundairyfarm.com/farmimages/farm%20pics%20%282%29_edited.jpg"
     }
   ];
