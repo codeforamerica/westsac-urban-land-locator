af3eb880ac6a0f2d588f422d3ce7429fb5f6be4c
Grant Smith <grantrobertsmith@gmail.com>
2015-05-15 18:44:37 -0700

    calculate acreage automatically, don't require it to be entered.

--- farmsList/farmsList/assets.py
+++ farmsList/farmsList/assets.py
@@ -14,6 +14,7 @@ css = Bundle(
 js = Bundle(
     "libs/jQuery/dist/jquery.js",
     "https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.3.14/angular.min.js",
+    "https://api.tiles.mapbox.com/mapbox.js/plugins/turf/v2.0.0/turf.min.js",
     # "libs/angular-mapbox/dist/angular-mapbox.min.js",  # <-- use this if they have accepted all of our changes
     "js/angular-mapbox.js",  # <-- use this while we need customizations, delete comments and this file otherwise
     "libs/bootstrap/dist/js/bootstrap.js",
