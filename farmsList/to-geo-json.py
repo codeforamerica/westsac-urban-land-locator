581a7d5d6d7b68240289a2495ee192770648987e
Grant Smith <grantrobertsmith@gmail.com>
2015-06-10 11:59:19 -0700

    add the parcels and migration script update just for dev stuff

--- farmsList/to-geo-json.py
+++ farmsList/to-geo-json.py
@@ -1,9 +1,5 @@
-import os
 import json
 from sqlalchemy import create_engine
-import sqlalchemy
-
-dirName = os.path.dirname(os.path.realpath(__file__))
 
 engine = create_engine('postgresql://farmslistadmin:@localhost/farms_list')
 conn = engine.connect()
@@ -11,7 +7,7 @@ parcels = []
 
 for x in range(0, 17):
 	filename = 'parcels{0:02d}.json'.format(x)
-	file = open(filename)
+	file = open("parcels-pristine/" + filename)
 	array = json.loads(file.read())
 	for y in range(0, len(array)):
 		type = 'MultiPolygon' if len(array[y]['shape']['geometry']['rings']) > 1 else 'Polygon'
@@ -23,12 +19,8 @@ for x in range(0, 17):
 		parcel['center'] = centerString
 		size = round(float(array[y]['acres']), 2)
 		size = size if size >= 0.01 else 0.01
-		size = size if size < 100 else 99.99
 		parcel['size'] = size
 		parcels.append(parcel)
 
 for parcel in parcels:
-	try:
-		conn.execute("INSERT INTO parcels (geometry, size, zoning, center) VALUES ('{}', {}, 'Ag', '{}')".format(parcel['geometry'], parcel['size'], parcel['center']))
-	except sqlalchemy.exc.DataError:
-		print "parcel geojson is too long, probably"
+	conn.execute("INSERT INTO parcels (geometry, size, zoning, center) VALUES ('{}', {}, 'Ag', '{}')".format(parcel['geometry'], parcel['size'], parcel['center']))
