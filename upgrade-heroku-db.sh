d089ff597d902eb796dfe134945a35f9b85b2e78
Grant <grantrobertsmith@gmail.com>
2015-08-14 22:02:36 -0700

    Create upgrade-heroku-db.sh
    this is pretty bogus of me, ultimately, we need to change the way db migrate works so that the output file is correct and db upgrade just works

---
+++ upgrade-heroku-db.sh
@@ -0,0 +1,1 @@
+heroku run 'python manage.py db init;python manage.py db migrate;STR=$(printf "import geoalchemy2\n\n");echo "$STR" > tmp.txt;FILE="`ls migrations/versions/*.py`";echo "`cat tmp.txt $FILE`" > `ls migrations/versions/*.py`;rm migrations/versions/*.pyc;sed -i -e "s/op.drop/# op.drop/g" $FILE;python -m compileall migrations/versions/;python manage.py db upgrade'
