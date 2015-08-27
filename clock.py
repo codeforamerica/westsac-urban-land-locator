57bca310eb1089e7260d9bca8d6d94e0de1f7772
Grant Smith <grantrobertsmith@gmail.com>
2015-08-27 16:04:54 -0700

    test queueing jobs on heroku

---
+++ clock.py
@@ -0,0 +1,22 @@
+from apscheduler.schedulers.blocking import BlockingScheduler
+from rq import Queue
+from worker import conn
+
+q = Queue(connection=conn)
+sched = BlockingScheduler()
+
+def every_three_minutes():
+	print('This job is run every three minutes.')
+
+def every_weekday_at_5pm():
+    print('This job is run every weekday at 5pm.')
+
+@sched.scheduled_job('interval', minutes=3)
+def timed_job():
+	q.enqueue(every_three_minutes)
+
+@sched.scheduled_job('cron', day_of_week='mon-fri', hour=17)
+def scheduled_job():
+	q.enqueue(every_weekday_at_5pm)
+
+sched.start()
