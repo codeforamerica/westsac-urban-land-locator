b7f56fcb3ac24ce8a6e161283ab411173c94c471
Imanol Aranzadi <iiaranzadi@gmail.com>
2015-09-14 10:54:12 -0700

    front-end style

--- farmsList/assets.py
+++ farmsList/assets.py
@@ -8,6 +8,7 @@ css = Bundle(
     "css/nav.css",
     "css/footer.css",
     "css/map.css",
+    "css/grayscale.css",
     filters="cssmin",
     output="public/css/common.css"
 )
@@ -20,7 +21,10 @@ js = Bundle(
     "https://api.tiles.mapbox.com/mapbox.js/plugins/turf/v2.0.0/turf.min.js",
     # "libs/angular-mapbox/dist/angular-mapbox.min.js",  # <-- use this if they have accepted all of our changes
     "js/angular-mapbox.js",  # <-- use this while we need customizations, delete comments and this file otherwise
+    "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0-alpha1/jquery.min.js",
     "libs/bootstrap/dist/js/bootstrap.min.js",
+    "https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js",
+    "js/grayscale.js",
     "js/plugins.js",
     "js/app.js",
     filters='jsmin',
