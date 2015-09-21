c4de8b8f9bd3835d70fbb1cd2535d244eae30c37
Grant Smith <grantrobertsmith@gmail.com>
2015-09-21 11:38:26 -0700

    test import of usda food desert data

--- clock.py
+++ clock.py
@@ -8,7 +8,7 @@ logging.basicConfig()
 q = Queue(connection=conn)
 sched = BlockingScheduler()
 
-@sched.scheduled_job('cron', hour=8, minute=15)  # This is UTC and translates to 1:15 AM PDT (12:15 AM PST)
+@sched.scheduled_job('cron', hour=18, minute=41) # hour=8, minute=15)  # This is UTC and translates to 1:15 AM PDT (12:15 AM PST)
 def scheduled_job():
 	q.enqueue(every_night_at_1am)
 
